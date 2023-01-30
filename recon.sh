f2e329ed13841416ec74c5d306af975da26e053a
İhsan Alp Pekel <87542668+alppekel@users.noreply.github.com>
2023-01-30 21:42:13 +0300

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -40,4 +40,57 @@ touch ~/$domainName/urls/waybackdata/s3scanner-buckets.txt
 
 # enumerating subdomains with ctfr.py
 
-python3 
+python3 ~/tools/py-scripts/ctfr.py -d $domainName -o domains_temp.txt
+
+# enumerating subdomains with subfinder
+
+subfinder -d $domainName -o domains_temp.txt
+
+# enumerating subdomains with assetfinder
+
+./assetfinder -subs-only $domainName >> domains_temp.txt
+
+# enumerating subdomains from wildcard domains
+
+cat domains_temp.txt |grep "*." >> wildcardDomains.txt
+cat wildcardDomains.txt | ./assetfinder -subs-only >> domains_temp.txt
+
+# cleanup - removing duplicates and wildcard domains
+
+sort -u domains_temp.txt | awk '!/\*/' >> subdomains.txt
+rm domains_temp.txt
+
+#####################################################################
+
+######################## enumerating github #########################
+
+github-subdomains -d $domainName -t >> ~/$domainName/github-data.txt
+
+#####################################################################
+
+######################## finding live subdomains ####################
+
+cat subdomains.txt | httprobe -c 50 -t 3000 | sort -u >> webservers.txt
+
+#####################################################################
+
+######################## enumerating urls ###########################
+
+# enumerating urls from waybackdata
+
+cat webservers.txt | waybackurls >> $domainName/waybackdata/waybackurl.txt
+
+#####################################################################
+
+#################### enumerating cloud assets #######################
+
+# enumeration with cloud_enum
+
+
+
+# enumerating buckets
+
+
+
+
+
