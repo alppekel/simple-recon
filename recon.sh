f6974242eba79f1946914a1234550a4f5ae235dd
İhsan Alp Pekel <87542668+alppekel@users.noreply.github.com>
2023-02-01 13:43:50 +0300

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -61,7 +61,7 @@ cat ~/$fileName/subdomains.txt | httprobe -c 50 -t 3000 | sort -u >> ~/$fileName
 
 ######################## enumerating urls ###########################
 
-echo "enumerating urls with getallurls (gau)"
+echo "enumerating urls with getallurls (gau) - this may take some time"
 cat ~/$fileName/webservers.txt | gau --threads 5 >> ~/$fileName/urls/gau-urls.txt
 
 #####################################################################
